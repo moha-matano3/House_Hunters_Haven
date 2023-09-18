fb402ccd22e6dd6e0071f90622ed7daa9f2c6586
Mohamed Nyaa Matano <Moha_Matano@Mohas-MacBook-Air.local>
2023-09-18 15:50:54 +0300

    Navbar of the house-hunting started... majorly drop down menus

---
+++ resources/views/home.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>House Hunters' Haven</title>
+
+    <style>
+        *
+        {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        .navbar
+        {
+            background: #7c6e7f;
+            text-align: center;
+        }
+        .navbar ul
+        {
+            display: inline-flex;
+            list-style: none;
+            color: #000000;
+        }
+        .navbar ul li 
+        {
+            width: 400px;
+            margin: 15px;
+            padding: 15px;
+        }
+        .navbar ul li a
+        {
+            text-decoration: none;
+            color: #000000;
+        }
+        .navbar ul li a:hover
+        {
+            color: #fff;
+        }
+        .subMenu {
+            width: 300px;
+            margin: 0px;
+            display: none;
+        }
+        .navbar ul li:hover .subMenu {
+            display: block;
+            position: absolute;
+            margin-top: 15px;
+            margin-left: -15px;
+            background: #7c6e7f;
+        }
+        .navbar ul li:hover .subMenu ul {
+            display: block;
+        }
+        .navbar ul li:hover .subMenu ul li {
+            width: auto;
+            padding: 10px;
+            border-bottom: 1px dotted #000;
+            background: transparent;
+            border-radius: 0;
+            text-align: left;
+        }
+        .navbar ul li:hover .subMenu ul li:last-child {
+            border-bottom: none;
+        }
+        .navbar ul li:hover .subMenu ul li:hover {
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+        <ul>
+            <li><a href=""> Menu </a>
+                <div class="subMenu">
+                    <ul>
+                        <li>Prize</li>
+                        <li><a href="">Rental</a></li>
+                        <li><a href="">To buy</a></li>
+                        <li class="MenuList">Bedrooms
+                            <div class="subMenu2">
+                                <ul>
+                                    <li><input type="checkbox"> Single room</li>
+                                    <li><input type="checkbox"> Studio/bedsitter</li>
+                                    <li><input type="checkbox"> 1 Bedroom</li>
+                                    <li><input type="checkbox"> 2 Bedrooms</li>
+                                    <li><input type="checkbox"> 3 Bedrooms</li>
+                                    <li><input type="checkbox"> 4 Bedrooms</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="MenuList">Amenities
+                            <div class="subMenu2">
+                                <ul>
+                                    <li><input type="checkbox"> WiFi</li>
+                                    <li><input type="checkbox"> Gated Community</li>
+                                    <li><input type="checkbox"> Un-gated Community</li>
+                                    <li><input type="checkbox"> GYM </li>
+                                    <li><input type="checkbox"> Hired security</li>
+                                    <li><input type="checkbox"> Swimming pool</li>
+                                    <li><input type="checkbox"> Provided parking space</li>
+                                    <li><input type="checkbox"> Fully furnished</li>
+                                </ul>
+                            </div>
+                        </li> 
+                        <li class="MenuList">Size
+                        <div class="subMenu2">
+                                <ul>
+                                    <li><input type="checkbox"> < than 500sqft </li>
+                                    <li><input type="checkbox"> < than 1500sqft</li>
+                                    <li><input type="checkbox"> < than 3000sqft</li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li><input type="text" placeholder="Enter Property name or location"> <input type="submit" value="Search"></li>
+            <li> <a href= ""> Profile </a>
+                <div class="subMenu">
+                    <ul>
+                        <li>Sign up</li>
+                        <li>Log in</li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
